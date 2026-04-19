c18310627bfdc5674d04f4330991ff6124f5745e
AryanSwami18 <aryanswami304@gmail.com>
2026-04-19 14:32:38 +0530

    added  cors config

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: "doodle-dash-frontend-3ef7.vercel.app",
         methods: ["GET", "POST"],
     },
 });
